47d89e3df4339adb062fabe95fc8b7e0ae37e587
gustavo-silva-clear <gustavo.silva.clear@gmail.com>
2023-02-13 10:48:44 -0300

    feat:variables and operators in JavaScript

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+Variaveis no JS
+
+
+String = 'texto';
+Number = 'inteiros ou decimais, 10 ou 1.4';
+
+Array = 'armazena mais de um valor em seus indices'
+TypeOf = mostra o tipo da variavel
+InstanceOf = Confima a instancia do array 
+ 
+Boolean = true or false, 0 1 ...
+
+ 
+
+funções de converção 
+
+parseInt, parseFloat, toString 
+
+Operadores
+
+= atribuição 
+== comparação
+=== compara o valor e o tipo de dados
+!= compara apenas os valores são diferentes
+!== compara se são diferentes os valores e os tipos de variaveis 
+
+*/ 
+
+// Colocando na pratica
+let a = 10;
+const b = 3;
+console.log(a == b);
+
+let c = 10;
+const d = "10";
+console.log(c == d);
+
+let e = 10;
+const f = "10";
+console.log(e === f);
+
+console.log(a != b);
+console.log(c !== d);
