ecd7085fcea7480397d3d7894d03f45c53c62bc0
gustavo-silva-clear <gustavo.silva.clear@gmail.com>
2023-02-14 08:55:02 -0300

    Working with arrays and developing forEach

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-/* Trabalhando manipulação de data em diferentes formatos */
+/* trabalhando com arrays */
 
-let now  = new Date();
-console.log(now);//mostra a data em segundos desde 1970
+let cars = ["Supra" , "GTR" , "RX-7" , new Date(), function(){}];
 
-console.log(now.getDate());//mostra o dia 
-console.log(now.getFullYear());// mostra o ano
-console.log(now.getMonth());//mostra o mês
+console.log(cars);//Mostra todo o conteudo do array
+console.log(cars.length);//mostra a quantidade de indices
+console.log(cars [2]);//mostra um indice especifico
+console.log(cars[3].getFullYear());//extrai o ano de dentro de uma função que está no indice 
+console.log(cars[4]());//chamo uma função que está em um indice
 
-console.log(now.toLocaleDateString("pt-br"));// mostra a data de acordo com cada local do mundo basta especificar
+cars.forEach(function(value , index){
+
+ console.log(index , value);
+
+})//forEach serve para percorrer o array ele mostra todos os items que compoem o array
