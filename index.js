18688360a1db52ff714096728032cd4586921f5a
gustavo-silva-clear <gustavo.silva.clear@gmail.com>
2023-02-14 10:34:24 -0300

    starting to build the calculator

--- index.js
+++ index.js
@@ -15,7 +15,76 @@ let celular =  function(){
 
     }
 
+}/* Orientação a objeto e funções de uma forma mais limpa 
+
+ O que é orientação a objeto, é baseado em uma classe com atributos e motodos, o metodo tem que executar algo, e pra isso é utilizado o termo "this" 
+
+
+
+*/
+
+/*______________________Forma antiga de se declarar uma classe_______________________________________
+
+//isso é um metodo
+
+let celular =  function(){
+
+
+
+    this.cor = "prata";//isso é um atributo
+
+    
+
+    this.ligar = function(){
+
+
+        console.log("uma ligação!");
+
+        return "ligando...";
+
+
+
+    }
+
+
+
 }
+
+
+
+let objeto = new celular();
+
+
+
+console.log(objeto);
+
+console.log(objeto.cor);
+
+console.log(objeto.ligar());
+
+_______________________________________________________________________________________________________*/
+
+
+
+/*_____________________Nova forma de declarar uma classe_________________________________________________
+
+
+
+class celular {
+
+
+
+    constructor()
+
+{
+
+        this.cor = "prata";
+
+}
+
+ligar(){
+
+    console.log("uma ligação!");
 
 let objeto = new celular();
 
---
+++ projetoCalculadora/scripts/controller/calcController.js
@@ -0,0 +1,12 @@
+class CalcController {
+
+constructor()
+{
+    this._displayCalc = "0";
+    this._dataAtual = "0";
+
+
+}
+
+}
+//classe = conjunto de atributos e metodos
