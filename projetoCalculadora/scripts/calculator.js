31b02fc36ab653dda7d94d999cb599a8b033f464
gustavo-silva-clear <125267517+gustavo-silva-clear@users.noreply.github.com>
2023-02-15 07:54:53 -0300

    Delete projetoCalculadora directory

--- projetoCalculadora/scripts/calculator.js
+++
@@ -1,7 +0,0 @@
-window.calculator = new CalcController // instacia uma variavel para receber os valores da calcController
-
-//Model - Manipula os dados 
-//View - interação com o usuario
-//Controller - define toda regra de negocio
-//MVC
-
